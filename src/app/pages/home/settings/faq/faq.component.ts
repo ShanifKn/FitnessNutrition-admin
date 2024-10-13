4f756477c313656782d6a6cccb55db59df869288
sheheem-bancapp <sheheem@bancapp.in>
2024-10-13 19:00:06 +0530

    faq management

--- src/app/pages/home/settings/faq/faq.component.ts
+++ src/app/pages/home/settings/faq/faq.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { AccordionModule } from 'primeng/accordion';
+import { DialogService, DynamicDialogModule, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { AddModalComponent } from './modals/add-modal/add-modal.component';
 
 @Component({
   selector: 'app-faq',
   standalone: true,
-  imports: [],
+  imports: [AccordionModule, DynamicDialogModule],
   templateUrl: './faq.component.html',
-  styleUrl: './faq.component.scss'
+  styleUrl: './faq.component.scss',
+  providers: [DialogService]
 })
 export class FaqComponent {
+
+  ref: DynamicDialogRef | undefined;
+
+  constructor(public dialogService: DialogService) {}
+
+  onAdd() {
+    this.ref = this.dialogService.open(AddModalComponent, {
+      header: 'Add Question',
+      width: '50%',
+      dismissableMask: true,
+    })
+  }
 
 }
